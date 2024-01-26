9ccea78a48cf78cae54b294ed4a09be02e3ca49d
Yağmur KAYA <101890851+yagmurkyaa@users.noreply.github.com>
2024-01-26 19:05:53 +0300

    Add files via upload

---
+++ Kosul_dongu_odev.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# **Ödev-1:** Kullanıcıdan maaş bilgisini istenir ve bu bilgiye göre maaşından ne kadar vergi kesileceğini hesaplanır. Kullanıcının geliri;
+# 
+# 1. 10000 ve altındaysa maaşından %5 kesinti olur.
+# 2. 25000 ve altındaysa maaşından %10 kesinti olur.
+# 3. 45000 ve altındaysa maaşından %25 kesinti olur.
+# 4. Diğer koşullarda %30 kesinti olur.
+# 
+# Bu durumlara göre kullanıcının yeni maaşı yazdırılır.
+
+# In[ ]:
+
+
+
+
+
+# **Ödev-2:** Kullanıcıdan kullanıcı adı ve şifre oluşturmasını istenir. Şifrenin uzunluğu altı haneye ulaşmışsa hesabınız oluşturuldu mesajı alınır, altı haneden azsa altı haneli şifre oluşturması gerektiğinin mesajı alınır. *(Sadece koşul kullanılması yeterli.)*
+
+# In[ ]:
+
+
+
+
+
+# **Ödev-3:** Bir önceki örnek geliştirilir.
+# 
+# 1. Kullanıcı girdiği şifre 5 ve 10 hane arasında olmak zorunda.
+# 2. Eğer bu koşula uyuyorsa "Hesabınız oluşturuldu." mesajı alır.
+# 3. Koşulu sağlamıyorsa "Lütfen girdiniz şifre 5 haneden az 10 haneden fazla olmasın!" uyarısı alır.
+# 4. Bunu oluştururken kullanıcı istediğimiz şartlarda şifre oluşturana kadar sormaya devam eder
+
+# In[ ]:
+
+
+
+
+
+# **Ödev-4:** Kullanıcıdan isim ve şifre isteyeceğiz ve şifre girişi için üç hak verilir.
+# 
+# 1. Eğer önceden tanımlı şifre ile kullanıcıdan gelen şifre aynıysa "Giriş yapıldı." yazar.
+# 2. Şifre girişi yanlışsa "Yanlış şifre girildi!" uyarısı verilsin ve üç yanlış denemede program biter.
+# 3. Tercihe göre kalan hak bilgisi verilir.
+
+# In[ ]:
+
+
+
+
